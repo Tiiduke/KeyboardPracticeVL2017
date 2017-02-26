3e4ff23b40b88f443b7982eca05de0c0e9eee478
K-W S <karl-walter@hotmail.com>
2017-02-26 20:21:57 +0200

    Linking
    Added a small update that demonstrates 2 additional links in the welcome
    message, that show template webpages.

--- application/views/welcome_message.php
+++ application/views/welcome_message.php
@@ -22,8 +22,13 @@ defined('BASEPATH') OR exit('No direct script access allowed');
 
 		<p>The corresponding controller for this page is found at:</p>
 		<code>application/controllers/Welcome.php</code>
+		
 
 		<p>If you are exploring CodeIgniter for the very first time, you should start by reading the <a href="user_guide/">User Guide</a>.</p>
+		<p>If you want to search for templates: try searching "W3Schools theme" or such, for example, here is a template: <a href="test_template1.php/">'webpage'</a>.</p>
+		<p>Of course, there are more pages to sample through: <a href="test_template2.php/">How about 'blog'?</a>.</p>
+		<p>Here is a webpage where you can learn to make a custom theme: <a href="https://www.w3schools.com/bootstrap/bootstrap_templates.asp">Just be careful with it</a>.</p>
+		
 	</div>
 
 	<p class="footer">Page rendered in <strong>{elapsed_time}</strong> seconds. <?php echo  (ENVIRONMENT === 'development') ?  'CodeIgniter Version <strong>' . CI_VERSION . '</strong>' : '' ?></p>
