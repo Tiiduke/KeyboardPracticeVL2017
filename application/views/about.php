e9997c8ff03a7b7690aab7b94febfec0472249a7
indrekln <laan.indrek@gmail.com>
2017-03-11 05:29:59 +0200

    Small corrections

--- application/views/about.php
+++ application/views/about.php
@@ -47,7 +47,7 @@ defined('BASEPATH') OR exit('No direct script access allowed');
 	<h1>About us:</h1>
 	
 	<div id="body">
-		<p>This is a poll environment created for the WebApp Development course of the University of Tartu, throught the spring of 2016/2017. afnakdvnsdmefecnsmkla</p>
+		<p>This is a poll environment created for the WebApp Development course of the University of Tartu, throught the spring of 2016/2017.</p>
 	</div>
 
 	
