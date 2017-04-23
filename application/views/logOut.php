b66d90ea6490a744b2a8e4732297000a9dfb2db7
K-W S <karl-walter@hotmail.com>
2017-04-23 15:32:16 +0300

    non working button in the non working link of the non working logout

---
+++ application/views/logOut.php
@@ -0,0 +1,18 @@
+
+<!--<head>
+	<meta name="viewport" content="width=device-width, initial-scale=1">
+	<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css">
+	<script src="https://ajax.googleapis.com/ajax/libs/jquery/1.12.4/jquery.min.js"></script>
+	<script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js"></script>
+</head>-->
+
+
+
+<div class="container">
+
+		<p>Confirm logout? vaja panna lang kutse</p>
+        <button type="button" class="btn btn-default btn-sm">
+          <span class="glyphicon glyphicon-log-out"></span> Exit ka siia lang
+        </button>
+
+</div>
