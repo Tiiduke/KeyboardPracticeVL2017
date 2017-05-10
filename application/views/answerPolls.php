4e6d6781610bec5916c57f731ba020cb2837f21b
indrekln <laan.indrek@gmail.com>
2017-05-10 16:13:55 +0300

    Revert "Answer Polls data saved to database"
    This reverts commit e6f91795924d850f1d49cc8d8bf44598f5b29aba.

--- application/views/answerPolls.php
+++ application/views/answerPolls.php
@@ -1,12 +1,9 @@
 <div id="answerPollsTable">
 	<?php
 		$email = $_SESSION['email'];
-		$userid = $_SESSION["userid"];
-		
 		if(!isset($_SESSION)) { 
 			session_start();
 		}
-		
 		include 'dbConnect.php';
 		
 		if ($email == "") {
@@ -20,7 +17,7 @@
 				$sqlgetquestion = "SELECT Question, PollID, Option1, Option2, Option3, Option4, Option5 FROM Questiontest WHERE PollID = '$pollid' AND Language = '$language'";
 				$getpollsresult = $conn->query($sqlgetquestion);
 				if ($getpollsresult->num_rows > 0) {
-					echo '<table class="table table-hover">';
+					echo '<table class="table table-striped table-bordered table-hover">';
 					echo "<tr><th>PollID:</th><th>Question:</th><th>Options:</th></tr>"; 
 					// output data of each row
 					while($row = $getpollsresult->fetch_assoc()) {
@@ -44,12 +41,30 @@
 				}
 			} else {
 				echo lang('AnswerThanks');
-				
-				$pollid = $_GET['pollid'];
-				$language = lang('Language');
-				$answer = $_GET['option'];
-				$sqlsaveanswer = "INSERT INTO Answerstest (AnswerID, UserID, PollID, Language, Answer) VALUES (NULL, '$userid', '$pollid', '$language', '$answer')";
-				$conn->query($sqlsaveanswer);
+				/*$sqlanswers = "SELECT Polltest.PollID, Usertest.Email, Polltest.Category, Questiontest.Question, Answerstest.Answer FROM Usertest INNER JOIN ((Polltest INNER JOIN Answerstest ON Polltest.PollID = Answerstest.PollID) INNER JOIN Questiontest ON Polltest.PollID = Questiontest.PollID) ON Usertest.UserID = Polltest.UserID WHERE Usertest.Email = '$email' AND Usertest.Language = Questiontest.Language ORDER BY Polltest.PollID ASC";
+				$answersresult = $conn->query($sqlanswers);
+				if ($answersresult->num_rows > 0) {
+					echo lang("YourPollsAnswers");
+					echo '<table class="table table-striped table-bordered table-hover">';
+					echo "<tr><th>PollID:</th><th>Author:</th><th>Category:</th><th>Question:</th><th>Answer:</th></tr>"; 
+					// output data of each row
+					while($row = $answersresult->fetch_assoc()) {
+						echo "<tr><td>"; 
+						echo $row['PollID'];
+						echo "</td><td>";   
+						echo $row['Email'];
+						echo "</td><td>";    
+						echo $row['Category'];
+						echo "</td><td>";    
+						echo $row['Question'];
+						echo "</td><td>";    
+						echo $row['Answer'];
+						echo "</td></tr>";  
+					}
+					echo '</table>';
+				} elseif ($pollsresult->num_rows == 0){
+					echo lang("YourPollsNone");
+				}*/
 
 			}
 
