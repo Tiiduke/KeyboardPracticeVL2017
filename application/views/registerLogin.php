94daf79342312d9b9f231db5aa476736d9a23869
indrekln <laan.indrek@gmail.com>
2017-05-21 23:45:17 +0300

    registerLogin validation fixes

--- application/views/registerLogin.php
+++ application/views/registerLogin.php
@@ -1,15 +1,15 @@
 <?php
 echo '<br><br>';
-echo '<div id="login-content">';
+echo '<div id="login-content2">';
 echo '<form action="' . htmlspecialchars($_SERVER["PHP_SELF"]) . '" method="post">';
-echo '<fieldset id="inputs">';
-echo '<label for="username">' . lang("UE") . '</label><br>';
-echo '<input id="username" type="email" name="Email" placeholder=' . lang("UEIn") . ' required><br><br>';  
-echo '<label for="password">' . lang("Par") . '</label><br>';
-echo '<input id="password" type="password" name="Password" placeholder=' . lang("ParIn") . ' required>';
+echo '<fieldset id="inputs2">';
+echo '<label for="username2">' . lang("UE") . '</label><br>';
+echo '<input id="username2" type="email" name="Email" placeholder=' . lang("UEIn") . ' required><br><br>';  
+echo '<label for="password2">' . lang("Par") . '</label><br>';
+echo '<input id="password2" type="password" name="Password" placeholder=' . lang("ParIn") . ' required>';
 echo '</fieldset>';
-echo '<fieldset id="actions">';
-echo '<input type="submit" id="submit" value="Log in">';
+echo '<fieldset id="actions2">';
+echo '<input type="submit" id="submit2" value="Log in">';
 echo '</fieldset>';
 echo '</form>';
 echo '</div>';                     
